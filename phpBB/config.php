ffca2d02b2281c3aeb0d31068d6a437a6807e4ab
Wardy-uk <44201141+Wardy-uk@users.noreply.github.com>
2020-05-17 15:42:14 +0100

    Add files via upload

---
+++ phpBB/config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = '10.169.0.236';
+$dbport = '';
+$dbname = 'charnwoo2_v58h';
+$dbuser = 'charnwoo2_v58h';
+$dbpasswd = 'nppiuh38utq5km97';
+$table_prefix = 'j5bq_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
